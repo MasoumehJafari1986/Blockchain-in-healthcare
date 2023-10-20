f5ff97b0e8ee12750b3d8d7d8b7396d4af9689f0
Masoumeh Jafari <144215655+MasoumehJafari1986@users.noreply.github.com>
2023-10-20 12:02:13 +0330

    Add files via upload

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,150 +1,370 @@
 export const contractAddress = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
-
-export const abi =[
-	{
-		inputs: [
-			{
-				internalType: "string",
-				name: "name",
-				type: "string"
-			},
-			{
-				internalType: "string",
-				name: "dataHash",
-				type: "string"
-			}
-		],
-		name: "addPatient",
-		outputs: [],
-		stateMutability: "nonpayable",
-		type: "function"
-	},
-	{
-		inputs: [
-			{
-				internalType: "address",
-				name: "doctor",
-				type: "address"
-			}
-		],
-		name: "authorizeDoctor",
-		outputs: [
-			{
-				internalType: "address[]",
-				name: "",
-				type: "address[]"
-			}
-		],
-		stateMutability: "nonpayable",
-		type: "function"
-	},
-	{
-		inputs: [
-			{
-				internalType: "address",
-				name: "user",
-				type: "address"
-			}
-		],
-		name: "authorizeUser",
-		outputs: [
-			{
-				internalType: "address[]",
-				name: "",
-				type: "address[]"
-			}
-		],
-		stateMutability: "nonpayable",
-		type: "function"
-	},
-	{
-		inputs: [],
-		stateMutability: "nonpayable",
-		type: "constructor"
-	},
-	{
-		inputs: [
-			{
-				internalType: "address",
-				name: "patient",
-				type: "address"
-			},
-			{
-				internalType: "uint256",
-				name: "index",
-				type: "uint256"
-			}
-		],
-		name: "authorizedDoctors",
-		outputs: [
-			{
-				internalType: "address",
-				name: "",
-				type: "address"
-			}
-		],
-		stateMutability: "view",
-		type: "function"
-	},
-	{
-		inputs: [
-			{
-				internalType: "address",
-				name: "patient",
-				type: "address"
-			},
-			{
-				internalType: "uint256",
-				name: "index",
-				type: "uint256"
-			}
-		],
-		name: "authorizedUsers",
-		outputs: [
-			{
-				internalType: "address",
-				name: "",
-				type: "address"
-			}
-		],
-		stateMutability: "view",
-		type: "function"
-	},
-	{
-		inputs: [],
-		name: "getPatientData",
-		outputs: [
-			{
-				internalType: "string",
-				name: "",
-				type: "string"
-			}
-		],
-		stateMutability: "view",
-		type: "function"
-	},
-	{
-		inputs: [
-			{
-				internalType: "address",
-				name: "",
-				type: "address"
-			}
-		],
-		name: "patients",
-		outputs: [
-			{
-				internalType: "string",
-				name: "name",
-				type: "string"
-			},
-			{
-				internalType: "string",
-				name: "dataHash",
-				type: "string"
-			}
-		],
-		stateMutability: "view",
-		type: "function"
-	}
-];
+export const abi = [
+  {
+    inputs: [
+        {
+			internalType: "address",
+			name: "target",
+			type: "address"
+		}
+	],
+	name: "AddressEmptyCode",
+	type: "error"
+  },
+  {
+	inputs: [
+		{
+			internalType: "address",
+			name: "account",
+			type: "address"
+		}
+	],
+	name: "AddressInsufficientBalance",
+	type: "error"
+  },
+  {
+	inputs: [],
+	name: "FailedInnerCall",
+	type: "error"
+  },
+  {
+    inputs: [
+		{
+			internalType: "address",
+			name: "token",
+			type: "address"
+		}
+	],
+	name: "SafeERC20FailedOperation",
+	type: "error"
+  },
+  {
+	anonymous: false,
+	inputs: [
+		{
+			indexed: true,
+			internalType: "address",
+			name: "_doctor",
+			type: "address"
+		}
+	],
+	name: "DoctorAdded",
+	type: "event"
+  },
+  {
+	anonymous: false,
+	inputs: [
+		{
+			indexed: true,
+			internalType: "address",
+			name: "caller",
+			type: "address"
+		},
+		{
+			indexed: false,
+			internalType: "address[]",
+			name: "doctors",
+			type: "address[]"
+		}
+	],
+	name: "DoctorListShown",
+	type: "event"
+  },
+  {
+	anonymous: false,
+	inputs: [
+		{
+			indexed: false,
+			internalType: "uint256",
+			name: "fee",
+			type: "uint256"
+		}
+	],
+	name: "FeeSubtracted",
+	type: "event"
+  },
+  {
+	anonymous: false,
+	inputs: [],
+	name: "FeeSubtractionError",
+	type: "event"
+  },
+  {
+	anonymous: false,
+	inputs: [],
+	name: "InsufficientBalance",
+	type: "event"
+  },
+  {
+	inputs: [
+		{
+			internalType: "address",
+			name: "_doctor",
+			type: "address"
+		}
+	],
+	name: "addDoctor",
+	outputs: [],
+	stateMutability: "nonpayable",
+	type: "function"
+  },
+  {
+	inputs: [
+		{
+			internalType: "string",
+			name: "_transactionHash",
+			type: "string"
+		}
+	],
+	name: "addTransaction",
+	outputs: [],
+	stateMutability: "nonpayable",
+	type: "function"
+  },
+  {
+	inputs: [
+		{
+			internalType: "address",
+			name: "doctorAddress",
+			type: "address"
+		}
+	],
+	name: "removeDoctor",
+	outputs: [],
+	stateMutability: "nonpayable",
+	type: "function"
+  },
+  {
+	inputs: [
+		{
+			internalType: "address",
+			name: "_doctor",
+			type: "address"
+		},
+		{
+			internalType: "uint256",
+			name: "_amount",
+			type: "uint256"
+		}
+	],
+	name: "transferFee",
+	outputs: [],
+	stateMutability: "nonpayable",
+	type: "function"
+  },
+  {
+	inputs: [
+		{
+			internalType: "string",
+			name: "_profile",
+			type: "string"
+		}
+	],
+	name: "updatePatientProfile",
+	outputs: [],
+	stateMutability: "nonpayable",
+	type: "function"
+  },
+  {
+	inputs: [
+		{
+			internalType: "address",
+			name: "_feeToken",
+			type: "address"
+		}
+	],
+	stateMutability: "nonpayable",
+	type: "constructor"
+  },
+  {
+	inputs: [
+		{
+			internalType: "address",
+			name: "",
+			type: "address"
+		}
+	],
+	name: "doctors",
+	outputs: [
+		{
+			internalType: "bool",
+			name: "",
+			type: "bool"
+		}
+	],
+	stateMutability: "view",
+	type: "function"
+  },
+  {
+	inputs: [],
+	name: "getDoctorCount",
+	outputs: [
+		{
+			internalType: "uint256",
+			name: "",
+			type: "uint256"
+		}
+	],
+	stateMutability: "view",
+	type: "function"
+  },
+  {
+	inputs: [
+		{
+			internalType: "address",
+			name: "_patient",
+			type: "address"
+		}
+	],
+	name: "getPatient",
+	outputs: [
+		{
+			components: [
+				{
+					internalType: "string",
+					name: "name",
+					type: "string"
+				},
+				{
+					internalType: "bool",
+					name: "isPatient",
+					type: "bool"
+				},
+				{
+					internalType: "string",
+					name: "condition",
+					type: "string"
+				},
+				{
+					internalType: "address",
+					name: "owner",
+					type: "address"
+				},
+				{
+					internalType: "string",
+					name: "profile",
+					type: "string"
+				},
+				{
+					components: [
+						{
+							internalType: "uint256",
+							name: "id",
+							type: "uint256"
+						},
+						{
+							internalType: "string",
+							name: "hash",
+							type: "string"
+						}
+					],
+					internalType: "struct PatientData.Transaction[]",
+					name: "transactions",
+					type: "tuple[]"
+				}
+			],
+			internalType: "struct PatientData.Patient",
+			name: "",
+			type: "tuple"
+		}
+	],
+	stateMutability: "view",
+	type: "function"
+  },
+  {
+	inputs: [],
+	name: "getTransactionCount",
+	outputs: [
+		{
+			internalType: "uint256",
+			name: "",
+			type: "uint256"
+		}
+	],
+	stateMutability: "view",
+	type: "function"
+  },
+  {
+	inputs: [
+		{
+			internalType: "uint256",
+			name: "index",
+			type: "uint256"
+		}
+	],
+	name: "getTransactionHash",
+	outputs: [
+		{
+			internalType: "string",
+			name: "",
+			type: "string"
+		}
+	],
+	stateMutability: "view",
+	type: "function"
+  },
+  {
+	inputs: [
+		{
+			internalType: "address",
+			name: "_address",
+			type: "address"
+		}
+	],
+	name: "isDoctor",
+	outputs: [
+		{
+			internalType: "bool",
+			name: "",
+			type: "bool"
+		}
+	],
+	stateMutability: "view",
+	type: "function"
+  },
+  {
+	inputs: [
+		{
+			internalType: "address",
+			name: "_address",
+			type: "address"
+		}
+	],
+	name: "isPatientOwner",
+	outputs: [
+		{
+			internalType: "bool",
+			name: "",
+			type: "bool"
+		}
+	],
+	stateMutability: "view",
+	type: "function"
+  },
+  {
+	inputs: [],
+	name: "patientProfile",
+	outputs: [
+		{
+			internalType: "string",
+			name: "",
+			type: "string"
+		}
+	],
+	stateMutability: "view",
+	type: "function"
+  },
+  {
+	inputs: [],
+	name: "showDoctorList",
+	outputs: [
+		{
+			internalType: "address[]",
+			name: "",
+			type: "address[]"
+		}
+	],
+	stateMutability: "view",
+	type: "function"
+  }
+]
--- frontend/index.js
+++ frontend/index.js
@@ -1,93 +1,195 @@
-
-//import { ethers } from "./ethers.js";
 import { ethers } from "./ethers-5.6.esm.min.js"
 import { abi, contractAddress } from "./constants.js"
 
-// Connect to the local Ethereum network
+const DoctorButton = document.getElementById("DoctorButton")
+const PatientProfileButton = document.getElementById("PatientProfileButton")
+const TransferFeeButton = document.getElementById("TransferFeeButton")
+const addDoctorButton = document.getElementById("addDoctorButton")
+const removeDoctorButton = document.getElementById("removeDoctorButton")
+const getDoctorCountButton = document.getElementById("getDoctorCountButton")
+const showDoctorListButton = document.getElementById("showDoctorListButton")
+const updatePatientProfileButton = document.getElementById("updatePatientProfileButton")
+const addTransactionButton = document.getElementById("addTransactionButton")
+const getTransactionCountButton = document.getElementById("getTransactionCountButton")
+const getTransactionHashButton = document.getElementById("getTransactionHashButton")
+const transferFeeButton = document.getElementById("transferFeeButton")
 
-const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545")
+DoctorButton.onclick = Doctor
+PatientProfileButton.onclick = PatientProfile
+TransferFeeButton.onclick = TransferFee
+addDoctorButton.onclick = addDoctor
+removeDoctorButton.onclick = removeDoctor
+getDoctorCountButton.onclick = getDoctorCount
+showDoctorListButton.onclick = showDoctorList
+updatePatientProfileButton.onclick = updatePatientProfile
+addTransactionButton.onclick = addTransaction
+getTransactionCountButton.onclick = getTransactionCount
+getTransactionHashButton.onclick = getTransactionHash
+transferFeeButton.onclick = transferFee
+
+
+
+  // Initialize the provider, signer, and contract instance
+const provider = new ethers.providers.JsonRpcProvider()
 const signer = provider.getSigner()
 const contract = new ethers.Contract(contractAddress, abi, signer)
 
-const addPatientButton = document.getElementById("addPatientButton")
-const authorizeDoctorButton = document.getElementById("authorizeDoctorButton")
-const authorizeUserButton = document.getElementById("authorizeUserButton")
-const authorizedDoctorsButton = document.getElementById("authorizedDoctorsButton")
-const authorizedUsersButton = document.getElementById("authorizedUsersButton")
-const getPatientDataButton = document.getElementById("getPatientDataButton")
+// Example function calls
 
-addPatientButton.onclick = addPatient
-authorizeDoctorButton.onclick = authorizeDoctor
-authorizeUserButton.onclick = authorizeUser
-authorizedDoctorsButton.onclick = authorizedDoctors
-authorizedUsersButton.onclick= authorizedUsers
-getPatientDataButton.onclick = getPatientData
+async function Doctor() {
+  window.location.href = "Doctor.html"
+}
 
-// Function to add a patient
-async function addPatient(name, dataHash) {
-  const accounts = await signer.getAddress()
+async function PatientProfile() {
+  window.location.href = "PatientProfile.html"
+}
+
+async function TransferFee() {
+  window.location.href = "TransferFee.html"
+}
+
+async function addDoctor(doctorAddress) {
   if (typeof window.ethereum !== "undefined") {
-    try {
-      await contract.addPatient(name, dataHash, { from: accounts })
-      console.log("Patient added successfully")
-    } catch (error) {
-      console.error("Error adding patient:", error)
+    // Check if the doctorAddress is already in the list
+    const isDoctorAdded = await contract.isDoctorAdded(doctorAddress);
+    if (isDoctorAdded) {
+      console.log("Doctor already exists in the list");
+      return;
+    }
+
+    const tx = await contract.addDoctor(doctorAddress);
+    await tx.wait();
+    console.log("Doctor added successfully");
+  } else {
+    connectButton.innerHTML = "Please install MetaMask";
+  }
+}
+
+async function isDoctorAdded(doctorAddress) {
+  const count = await contract.getDoctorCount();
+  for (let i = 0; i < count; i++) {
+    const doctor = await contract.doctors(i);
+    if (doctor === doctorAddress) {
+      return true;
+    }
+  }
+  return false;
+}
+
+async function removeDoctor(doctorAddress) {
+  if (typeof window.ethereum !== "undefined") {
+    const isDoctorAdded = await contract.isDoctorAdded(doctorAddress);
+    if (isDoctorAdded) {
+      const tx = await contract.removeDoctor(doctorAddress);
+      await tx.wait();
+      console.log("Doctor removed successfully");
+    } else {
+      console.log("Doctor not found in the list");
     }
   } else {
-    connectButton.innerHTML = "Please install MetaMask"
+    connectButton.innerHTML = "Please install MetaMask";
   }
 }
 
-// Function to authorize a doctor
-async function authorizeDoctor(doctor) {
-  const accounts = await signer.getAddress()
+async function getDoctorCount() {
+  const count = await contract.getDoctorCount()
+  console.log(`There are ${count} doctors in the contract`)
+}
+
+async function showDoctorList() {
   try {
-    await contract.authorizeDoctor(doctor, { from: accounts })
-    console.log("Doctor authorized successfully")
+    const isOwner = await contract.owner() === signer.getAddress();
+    const isDoctor = await contract.doctors(signer.getAddress());
+    if (isOwner || isDoctor) {
+      const doctors = await contract.showDoctorList()
+      console.log("List of doctors:")
+      doctors.forEach((doctor, index) => {
+        console.log(`${index + 1}. ${doctor}`)
+      })
+      console.log("List of doctors showed.");
+    } else {
+      console.log("Only contract owner or doctors can show list of doctors.");
+    }
   } catch (error) {
-    console.error("Error authorizing doctor:", error)
+    console.error("Error showing doctor list:", error);
   }
 }
 
-// Function to authorize a user
-async function authorizeUser(user) {
-  const accounts = await signer.getAddress()
+async function updatePatientProfile(profile) {
   try {
-    await contract.authorizeUser(user, { from: accounts })
-    console.log("User authorized successfully")
+    const isOwner = await contract.owner() === signer.getAddress();
+    const isDoctor = await contract.doctors(signer.getAddress());
+    if (isOwner || isDoctor) {
+      const tx = await contract.updatePatientProfile(profile);
+      await tx.wait();
+      console.log("Patient profile updated successfully");
+    } else {
+      console.log("Only contract owner or doctors can update patient profiles");
+    }
   } catch (error) {
-    console.error("Error authorizing user:", error)
+    console.error("Error updating patient profile:", error);
   }
 }
 
-// Function to get the patient's data
-async function getPatientData() {
-  const accounts = await signer.getAddress()
+async function addTransaction(transactionHash) {
   try {
-    const dataHash = await contract.getPatientData({ from: accounts })
-    console.log("Patient data hash:", dataHash)
-    return dataHash
-  } catch (error) {
-    console.error("Error getting patient data:", error)
-  }
+    const isOwner = await contract.owner() === signer.getAddress();
+    const isDoctor = await contract.doctors(signer.getAddress());
+    if (isOwner || isDoctor) {
+      const tx = await contract.addTransaction(transactionHash);
+      await tx.wait();
+      console.log("Transaction added successfully");
+    }else {
+        console.log("Only contract owner or doctors can add transaction");
+      }
+    } catch (error) {
+      console.error("Error adding transaction:", error);
+    }
 }
 
-// Function to get the authorized doctor at a specific index
-async function authorizedDoctors(patient, index) {
+async function getTransactionCount() {
+  const count = await contract.getTransactionCount();
+  console.log(`There are ${count} transactions for the patient`);
+}
+
+async function getTransactionHash(index) {
   try {
-    return contract.authorizedDoctors(patient, index)
+    const isOwner = await contract.owner() === signer.getAddress();
+    const isDoctor = await contract.doctors(signer.getAddress());
+    if (isOwner || isDoctor) {
+      const hash = await contract.getTransactionHash(index);
+      console.log(`Transaction hash at index ${index}: ${hash}`);
+    } else {
+      console.log("Only contract owner or doctors can access this function");
+    }
   } catch (error) {
-    console.error("Error getting authorized doctor:", error)
+    console.error("Error getting transaction hash:", error);
   }
 }
 
-// Function to get the authorized user at a specific index
-async function authorizedUsers(patient, index) {
-  try {
-    return contract.authorizedUsers(patient, index)
-  } catch (error) {
-    console.error("Error getting authorized user:", error)
+async function transferFee(doctorAddress, amount) {
+  const transferFeeButton = document.getElementById("transferFeeButton");
+  transferFeeButton.innerHTML = "fee";
+
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    const balance = await contract.feeToken.balanceOf(signer.getAddress());
+    if (balance.lt(amount)) {
+      console.log("Insufficient balance");
+      return;
+    }
+
+    try {
+      const tx = await contract.transferFee(doctorAddress, amount);
+      await tx.wait();
+      console.log("Fee transferred successfully");
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    transferFeeButton.innerHTML = "Please install MetaMask";
   }
 }
-
-
